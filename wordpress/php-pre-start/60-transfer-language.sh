cafab59756180648196cba2d46b76f4ecd91ff42
Roland Thomas Lichti <rlichti@kaiserpfalz-edv.de>
2018-12-31 18:57:15 +0100

    repairs the transfer script. DOCUMENT_ROOT does not exist here. I hope APP_ROOT exists :-)

--- wordpress/php-pre-start/60-transfer-language.sh
+++ wordpress/php-pre-start/60-transfer-language.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
-if [ ! -f "${DOCUMENT_ROOT}/wp-content/languages/.installed" ] ; then
+if [ ! -f "${APP_ROOT}/src/wp-content/languages/.installed" ] ; then
     echo -n "Copying langauges ... "
 
-    if [ ! -d ${DOCUMENT_ROOT}/wp-content/languages ] ; then
+    if [ ! -d ${APP_ROOT}/src/wp-content/languages ] ; then
         echo -n "creating languages directory ... "
-        mkdir -p ${DOCUMENT_ROOT}/wp-content/languages
+        mkdir -p ${APP_ROOT}/src/wp-content/languages
     else
         echo -n "language directory exists ... "
     fi
 
     echo -n "copying ... "
-    cp -a ${DOCUMENT_ROOT}/../languages/* ${DOCUMENT_ROOT}/wp-content/languages
+    cp -a ${APP_ROOT}/languages/* ${APP_ROOT}/src/wp-content/languages
 
     echo -n "marking installed files ... "
-    touch ${DOCUMENT_ROOT}/wp-content/languages/.installed
+    touch ${APP_ROOT}/src/wp-content/languages/.installed
     echo "done"
 else
     echo "skipped"
