a47eda49ee0e570fa6ce992378abb7d1b8bf3b0b
Roland Thomas Lichti <rlichti@kaiserpfalz-edv.de>
2019-01-01 11:59:52 +0100

    cleans the 60-transfer-language.sh

--- wordpress/php-pre-start/60-transfer-language.sh
+++ wordpress/php-pre-start/60-transfer-language.sh
@@ -1,20 +1,28 @@
 #!/bin/sh
 
-if [ ! -f "${APP_ROOT}/src/wp-content/languages/.installed" ] ; then
-    echo -n "Copying langauges ... "
+SOURCE_DIR=${APP_ROOT}/languages
+TARGET_DIR=${APP_ROOT}/src/wp-content/languages
 
-    if [ ! -d ${APP_ROOT}/src/wp-content/languages ] ; then
-        echo -n "creating languages directory ... "
-        mkdir -p ${APP_ROOT}/src/wp-content/languages
-    else
-        echo -n "language directory exists ... "
+function create_directory() {
+    DIRECTORY=$1
+
+    if [ ! -d ${DIRECTORY} ] ; then
+        echo -ne "creating ${DIRECTORY} ... "
+        mkdir -p ${DIRECTORY}
     fi
+}
+
+
+echo -n "Copying languages to ${TARGET_DIR} ... "
+
+if [ ! -f "${TARGET_DIR}/.installed" ] ; then
+    create_directory ${TARGET_DIR}
 
     echo -n "copying ... "
-    cp -a ${APP_ROOT}/languages/* ${APP_ROOT}/src/wp-content/languages
+    cp -a ${SOURCE_DIR}/* ${TARGET_DIR}
 
     echo -n "marking installed files ... "
-    touch ${APP_ROOT}/src/wp-content/languages/.installed
+    touch ${TARGET_DIR}/.installed
     echo "done"
 else
     echo "skipped"
