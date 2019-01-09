b5bba1c89acf0e4a5f747364e99a8dd63e254e50
Roland T. Lichti <rlichti@kaiserpfalz-edv.de>
2019-01-09 07:58:07 +0100

    adds the language directories of all plugins to the persistent volumes.

--- wordpress/php-pre-start/60-transfer-language.sh
+++ wordpress/php-pre-start/60-transfer-language.sh
@@ -3,6 +3,9 @@
 SOURCE_DIR=${APP_ROOT}/languages
 TARGET_DIR=${APP_ROOT}/src/wp-content/languages
 
+PLUGIN_SOURCE_DIR=${APP_ROOT}/plugin-languages
+PLUGIN_DIR=${APP_ROOT}/src/wp-content/plugins
+
 function create_directory() {
     DIRECTORY=$1
 
@@ -27,3 +30,35 @@ if [ ! -f "${TARGET_DIR}/.installed" ] ; then
 else
     echo "skipped"
 fi
+
+pushd . &> /dev/null
+echo -n "Removing old plugin languages ... "
+create_directory ${APP_ROOT}/data/plugin-languages
+
+cd "${APP_ROOT}/data/plugin-languages"
+for PLUGIN in * ; do
+    if [ ! -d "${PLUGIN_DIR}/${PLUGIN}" ] ; then
+        echo "${PLUGIN} ... "
+        rm -rf "${PLUGIN}"
+    fi
+done
+
+echo -n "Copying plugin languags ... "
+cd ${PLUGIN_DIR}
+
+for PLUGIN in * ; do
+    if [ -d "${PLUGIN}/languages" -a ! -f "${PLUGIN}/languages/.installed" ] ; then
+        echo -n "copying ... "
+
+        cp "${PLUGIN_SOURCE_DIR}/${PLUGIN}"/* "${PLUGIN}/languages"
+
+        echo -n "marking plugin as installed ... "
+        touch "${PLUGIN}/languages/.installed"
+        echo "done"
+    else
+        echo "${PLUGIN} skipped ... "
+    fi
+done
+
+popd &>/dev/null
+echo "done"
