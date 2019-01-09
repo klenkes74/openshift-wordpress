4bed72e407ca5666ff9e3afc2a8612b1cfdb12e6
Roland T. Lichti <rlichti@kaiserpfalz-edv.de>
2019-01-09 09:21:29 +0100

    is it now working? 4

--- wordpress/php-pre-start/60-transfer-language.sh
+++ wordpress/php-pre-start/60-transfer-language.sh
@@ -2,12 +2,15 @@
 
 SOURCE_DIR=${APP_ROOT}/languages
 TARGET_DIR=${APP_ROOT}/src/wp-content/languages
+LANGUAGE_PERSISTENT_DIR=${APP_ROOT}/data/languages
 
 PLUGIN_SOURCE_DIR=${APP_ROOT}/plugin-languages
 PLUGIN_DIR=${APP_ROOT}/src/wp-content/plugins
+PLUGIN_PERSISTENT_DIR=${APP_ROOT}/data/plugin-languages
 
 THEME_SOURCE_DIR=${APP_ROOT}/theme-languages
 THEME_DIR=${APP_ROOT}/src/wp-content/themes
+THEME_PERSISTENT_DIR=${APP_ROOT}/data/theme-languages
 
 function create_directory() {
     DIRECTORY=$1
@@ -20,15 +23,15 @@ function create_directory() {
 
 
 echo -n "Copying languages to ${TARGET_DIR} ... "
-create_directory ${APP_ROOT}/data/languages
+create_directory ${LANGUAGE_PERSISTENT_DIR}
 if [ ! -f "${TARGET_DIR}/.installed" ] ; then
     create_directory ${TARGET_DIR}
 
     echo -n "copying ... "
-    cp -a ${SOURCE_DIR}/* ${TARGET_DIR}
+    cp -a "${SOURCE_DIR}"/* "${TARGET_DIR}"
 
     echo -n "marking installed files ... "
-    touch ${TARGET_DIR}/.installed
+    touch "${TARGET_DIR}"/.installed
     echo "done"
 else
     echo "skipped"
@@ -36,9 +39,9 @@ fi
 
 pushd . &> /dev/null
 echo -n "Removing old plugin languages ... "
-create_directory ${APP_ROOT}/data/plugin-languages
+create_directory ${PLUGIN_PERSISTENT_DIR}
 
-cd "${APP_ROOT}/data/plugin-languages"
+cd "${PLUGIN_PERSISTENT_DIR}"
 for PLUGIN in * ; do
     if [ ! -d "${PLUGIN_DIR}/${PLUGIN}" ] ; then
         echo "${PLUGIN} ... "
@@ -50,28 +53,29 @@ echo -n "Copying plugin languags ... "
 cd ${PLUGIN_DIR}
 
 for PLUGIN in * ; do
-    if [ -d "${PLUGIN}/languages" ] ; then
+    if [ -d "${PLUGIN_SOURCE_DIR}/${PLUGIN}/languages" ] ; then
         if [! -f "${PLUGIN}/languages/.installed" ] ; then
-            echo -n "copying '${PLUGIN}' ... "
+            create_directory "${PLUGIN_PERSISTENT_DIR}/${PLUGIN}"
 
+            echo -n "copy '${PLUGIN}' ... "
             cp "${PLUGIN_SOURCE_DIR}/${PLUGIN}"/* "${PLUGIN}/languages"
 
             echo -n "marking plugin as installed ... "
-            touch "${PLUGIN}/languages/.installed"
+            touch "${PLUGIN}"/languages/.installed
             echo "done"
         else
-            echo "'${PLUGIN}' skipped ... "
+            echo "'${PLUGIN}' alread installed"
         fi
     else
-        echo "'${PLUGIN}' skipped ... "
+        echo "'${PLUGIN}' has no language files"
     fi
 done
 
 
 echo -n "Removing old theme languages ... "
-create_directory ${APP_ROOT}/data/theme-languages
+create_directory "${THEM_PERSISTENT_DIR}"
 
-cd "${APP_ROOT}/data/theme-languages"
+cd "${THEME_PERSISTENT_DIR}"
 for THEME in * ; do
     if [ ! -d "${THEME_DIR}/${THEME}" ] ; then
         echo "${THEME} ... "
@@ -83,20 +87,21 @@ echo -n "Copying theme languags ... "
 cd ${THEME_DIR}
 
 for THEME in * ; do
-    if [ -d "${THEME}/languages" ] ; then
+    if [ -d "${THEME_SOURCE_DIR}/${THEME}/languages" ] ; then
         if [ ! -f "${THEME}/languages/.installed" ] ; then
-            echo -n "copying '${THEME}' ... "
+            create_directory "${THEME_PERSISTENT_DIR}/${PLUGIN}"
 
+            echo -n "copy '${THEME}' ... "
             cp "${THEME_SOURCE_DIR}/${THEME}"/* "${THEME}/languages"
 
             echo -n "marking theme as installed ... "
-            touch "${THEME}/languages/.installed"
+            touch "${THEME}"/languages/.installed
             echo "done"
         else
-            echo "'${THEME}' skipped ... "
+            echo "'${THEME}' alread installed"
         fi
     else
-        echo "'${THEME}' skipped ... "
+        echo "'${THEME}' has no language files"
     fi
 done
 
